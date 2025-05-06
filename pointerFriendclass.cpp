17c7e2948695cb100b28c4d07b92bd1a1017ed6f
Radiva279 <radivagalih12@gmail.com>
2025-05-06 10:53:18 +0700

    menambahkan perilaku / prosedur dengan akses public

--- pointerFriendclass.cpp
+++ pointerFriendclass.cpp
@@ -6,3 +6,12 @@ class PersegiPanjang
 public : // akses modifier
     int panjang, lebar;
 
+public :
+
+    void input()
+    {
+        cout << " Masukkan Panjang : ";
+        cin >> panjang;
+        cout << "Masukkan Lebar : ";
+        cin >> lebar;
+   }
