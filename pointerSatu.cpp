b3365255bd7818e28df988f79ee38eccb3a509f4
Radiva279 <radivagalih12@gmail.com>
2025-05-06 11:07:57 +0700

    membuat objek dan pointer references dan dereferences

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -9,3 +9,16 @@ class mahasiswa{
     }
 };
 
+int main(){
+
+    mahasiswa mhs{1}; //object mhs
+    mhs.showNim(); //member acces operator
+
+    mahasiswa &refMhs = mhs; //Pointer references refMhs
+    refMhs.nim = 2; //Member Acces Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //Pointer Dereferences pMhs
+    pMhs->showNim();
+    return 0;
+}
