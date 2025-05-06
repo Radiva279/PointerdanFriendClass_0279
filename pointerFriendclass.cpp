cefee035a16e66936380b446b9b57df4c9b746a2
Radiva279 <radivagalih12@gmail.com>
2025-05-06 10:55:03 +0700

    membuat class lingkaran dengan akses public

--- pointerFriendclass.cpp
+++ pointerFriendclass.cpp
@@ -24,3 +24,19 @@ public :
    Lingkaran ol;
 };
 
+class Lingkaran
+{
+public :
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan jari-jari : ";
+        cin>> jarijari;
+    }
+
+    int luasLingkaran(int r)
+    {
+        return 3.34 * r * r;
+    }
+};
